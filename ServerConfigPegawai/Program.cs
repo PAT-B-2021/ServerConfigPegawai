0a8f78015ec7540071819e7124923f064380e1f0
annis <nissinkentang@gmail.com>
2022-01-22 06:20:44 +0700

    Add project files.

---
+++ ServerConfigPegawai/Program.cs
@@ -0,0 +1,33 @@
+﻿using ServicePegawaiPAT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConfigPegawai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(Service1));
+                hostObjek.Open();
+                Console.WriteLine("Server ready");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
